384f0c71a349d07963dd4602eb5c6100cc684e79
Sandeep Sharma <sandeepsh9210@gmail.com>
2016-06-12 17:31:33 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-
-var ctx=document.getElementById("mycanvas1").getContext("2d");
+var c =document.getElementById("mycanvas1");
+var ctx=c.getContext("2d");
 var grd=ctx.createLinearGradient(0,0,300,0);
 grd.addColorStop(0,"white");
 grd.addColorStop(.1,"violet");
@@ -13,9 +13,9 @@ grd.addColorStop(.8,"pink");
 grd.addColorStop(.85,"brown");
 grd.addColorStop(.9,"grey");
 grd.addColorStop(1,"black");
+
 ctx.fillStyle=grd;
 ctx.fillRect(0,0,300,300);
-
 
  mycanvas1.onmouseup=function(e){
  var mousex=e.offsetX;
@@ -25,13 +25,14 @@ ctx.fillRect(0,0,300,300);
  var g=c[1];
  var b=c[2];
  
-document.getElementById("color").style.backgroundColor="rgb("+r+" , "+g+" , "+b+")";
+ document.getElementById("color").style.backgroundColor="rgb("+r+" , "+g+" , "+b+")";
 document.getElementById("i1").value=r;
 document.getElementById("i2").value=g;
 document.getElementById("i3").value=b;
 document.getElementById("hex").value=rgbToHex(r,g,b);
 move();
 history();
+document.getElementById("a2").value="1";
  }
 
 function show(){
@@ -39,14 +40,15 @@ document.getElementById("color").style.backgroundColor=document.getElementById("
 document.getElementById("i1").value= hexToR(document.getElementById("hex").value);
 document.getElementById("i2").value= hexToG(document.getElementById("hex").value);
 document.getElementById("i3").value= hexToB(document.getElementById("hex").value);
+
 function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
 function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
 function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
 function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 move();
 history();
+document.getElementById("a2").value="1";
 }
-
 
 function display(){
 var a=document.getElementById("i1").value;
@@ -56,8 +58,8 @@ move();
 document.getElementById("color").style.backgroundColor="rgb("+a+" , "+b+" , "+c+")";
 document.getElementById("hex").value=rgbToHex(a,b,c);
 history();
+document.getElementById("a2").value="1";
 }
-
 
 function move() {
 document.getElementById("x1").value=document.getElementById("i1").value;
@@ -76,12 +78,24 @@ function toHex(n) {
 
 var history1=["rgb(0,0,0)" ];
 function history(){
+document.getElementById("hist").innerHTML="";
+if (history1.length <= 10){
 history1.reverse();
 history1[history1.length]= "<br>" + document.getElementById("color").style.backgroundColor + "<br>";
 history1.reverse();
-document.getElementById("hist").innerHTML=history1;
-}
+ 
+ var table=document.getElementById("mytable");
+ var row=table.insertRow(0);
+var cell1=row.insertCell(0);
+var cell2=row.insertCell(1); 
+cell1.style.backgroundColor=document.getElementById("color").style.backgroundColor;
+cell1.style.width="25px";
+cell2.innerHTML=history1[0];
+}else{history1.pop();
+document.getElementById("mytable").deleteRow(9);
+ history();}
 
+}
 
 function change1(){
 var a=document.getElementById("x1").value;
@@ -93,12 +107,10 @@ document.getElementById("i3").value=c;
 display();
 }
 
-
 function adjust(){
 var m=document.getElementById("a2").value;
 var a=document.getElementById("i1").value;
 var b =document.getElementById("i2").value;
 var c =document.getElementById("i3").value;
 document.getElementById("color").style.background="rgba("+a+" , "+b+" , "+c+" , "+m+")";
-document.getElementById("a1").value=document.getElementById("a2").value;
 }
